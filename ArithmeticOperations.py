bb21585c4c07ed53c2e7b589f79e784832c16fa9
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2019-05-29 19:50:34 +0530

    Add files via upload

---
+++ ArithmeticOperations.py
@@ -0,0 +1,30 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>>     # Arithmetic operations
+    
+>>> 
+>>> 12+10
+22
+>>> 12-10
+2
+>>> 12*10
+120
+>>> 2*3
+6
+>>> 2 ** 3
+8
+>>> 2 * 2 * 2
+8
+>>> 12/10
+1.2
+>>> 12//10
+1
+>>> 12 % 10
+2
+>>> 12 | 10
+14
+>>> 1-1
+0
+>>> 01
+SyntaxError: invalid token
+>>> 
