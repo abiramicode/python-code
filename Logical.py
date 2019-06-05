373d8432bc3b14cbe1b781bb5f13ca0d3698c4fe
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2019-06-05 12:24:08 +0530

    Add files via upload

---
+++ Logical.py
@@ -0,0 +1,40 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Logical operations
+>>> 
+>>> 1 and 1
+1
+>>> 1 and 0
+0
+>>> 0 and 1
+0
+>>> 0 and 0
+0
+>>> 
+>>> 
+>>> 
+>>> 1 or 1
+1
+>>> 0 and 0
+0
+>>> 1 and 0
+0
+>>> 0 and 1
+0
+>>> 
+>>> not True
+False
+>>> not not True
+True
+>>> 
+>>> 
+>>> 
+>>> True and True
+True
+>>> True or True
+True
+>>> False or False
+False
+>>> False and False
+False
+>>> 
