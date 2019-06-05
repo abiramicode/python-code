c9e10d4a0a4afe4fd7e68b8a118051986666e699
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2019-06-05 12:23:47 +0530

    Add files via upload

---
+++ Relational.py
@@ -0,0 +1,40 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>>   #Relational operations
+>>> 
+>>> 
+>>> i=0
+>>> j=0
+>>> i<j
+False
+>>> i==j
+True
+>>> i!=j
+False
+>>> i<=j
+True
+>>> i>=j
+True
+>>> i>j
+False
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> a=89
+>>> b=123
+>>> 
+>>> a!=b
+True
+>>> a<=b
+True
+>>> a>=b
+False
+>>> a==b
+False
+>>> a<b
+True
+>>> a>b
+False
+>>> 
