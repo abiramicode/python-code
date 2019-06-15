e88e8152fa5bd596fd5e87083cb460097925b402
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2019-06-15 11:53:50 +0530

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,24 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # conversion
+>>> 
+>>> 
+>>> bin(24)
+'0b11000'
+>>> 
+>>> bin(36)
+'0b100100'
+>>> 
+>>> 
+>>> 
+>>> hex(255)
+'0xff'
+>>> 
+>>> hex(26)
+'0x1a'
+>>> 
+>>> 
+>>> oct(255)
+'0o377'
+>>> 
+>>> 
