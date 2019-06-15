43c0ef3852d9adb70d3b1a6cc7b4d07163ded8df
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2019-06-15 11:54:39 +0530

    Add files via upload

---
+++ userinput.py
@@ -0,0 +1,25 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # user input
+>>> 
+>>> x=input("Enter the value")
+Enter the value 2
+>>> type(x)
+<class 'str'>
+>>> 
+>>> 
+>>> x=int(input("Enter the value"))
+Enter the value 2
+>>> type(x)
+<class 'int'>
+>>> 
+>>> 
+>>> x=int(input("Enter the value"))
+Enter the value 2
+>>> y=int(input("Enter the value 1"))
+Enter the value 1 4
+>>> z=x+y
+>>> print(z)
+6
+>>> 
+>>> 
