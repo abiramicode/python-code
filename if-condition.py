779d717faccb0326262999c5ae1d77d1c6fa7e0a
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2019-07-12 11:44:24 +0530

    Add files via upload

---
+++ if-condition.py
@@ -0,0 +1,19 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> i=0
+>>> if(i<10):
+	print("i is less than 10")
+
+	
+i is less than 10
+>>> 
+>>> 
+>>> 
+>>> i=10
+>>> if(i<10):
+	print("i is less than 10")
+
+	
+>>> 
+>>> 
+>>> 
