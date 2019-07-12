ccbb81f8818ce4e0585f98b1b676c5cd1bac008f
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2019-07-12 11:45:45 +0530

    Add files via upload

---
+++ elif statement.py
@@ -0,0 +1,18 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # if elif else
+>>> 
+>>> if(False):
+	print("welcome")
+elif(False):
+	print("True")
+elif(True):
+	print("welcome to india")
+else:
+	print("hi abi")
+
+	
+welcome to india
+>>> 
+>>> 
+>>> 
