70b19866f0dde308caf75b02ec4c0ef378c9ce3d
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2019-07-13 11:01:49 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,24 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # while loop
+>>> 
+>>> while(False):
+	print()
+else:
+	print("am existing from loop")
+
+am existing from loop
+>>> 
+
+>>> 
+>>> 
+>>> while(False):
+	print("hi how are you")
+else:
+	print("am existing from loop")
+
+	
+am existing from loop
+>>> 
+>>> 
+>>> 
