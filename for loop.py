488d43ab7af8905522ed3909fb526137fecc2e94
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2019-07-13 11:01:19 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,161 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # for loop
+>>> 
+>>> for i in range(10):
+	print("abirami")
+
+	
+abirami
+abirami
+abirami
+abirami
+abirami
+abirami
+abirami
+abirami
+abirami
+abirami
+>>> 
+>>> 
+>>> for i in range(0,10):
+	print(i)
+
+	
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> 
+>>> 
+>>> for i in range(10,20):
+	print(i)
+
+	
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+>>> 
+>>> 
+>>> 
+>>> for i in range(100,120):
+	print(i)
+
+	
+100
+101
+102
+103
+104
+105
+106
+107
+108
+109
+110
+111
+112
+113
+114
+115
+116
+117
+118
+119
+>>> 
+>>> 
+>>> for i in range(100,120):
+	print(i*2)
+
+	
+200
+202
+204
+206
+208
+210
+212
+214
+216
+218
+220
+222
+224
+226
+228
+230
+232
+234
+236
+238
+>>> 
+>>> 
+>>> 
+>>> for i in range(20,40,2):
+	print(i)
+
+	
+20
+22
+24
+26
+28
+30
+32
+34
+36
+38
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> for i in range(0,4):
+	for j in range(0,3):
+		print(i)
+
+		
+0
+0
+0
+1
+1
+1
+2
+2
+2
+3
+3
+3
+>>> for i in range(0,4):
+	for j in range(0,3):
+		print(j)
+
+		
+0
+1
+2
+0
+1
+2
+0
+1
+2
+0
+1
+2
+>>> 
