3208baccbc5e5df3d33030bc37845850812772cd
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2019-07-17 11:24:20 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,23 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # function
+>>> 
+>>> def getdata(x,y):
+	return x,y
+
+>>> getdata(10,32)
+(10, 32)
+>>> x=getdata(2,3)
+>>> x
+(2, 3)
+>>> 
+>>> 
+>>> def getdata(x,y,z):
+	return x**y*z
+
+>>> getdata(2,3,2)
+16
+>>> 2**6
+64
+>>> 
+>>> 
