ae891da433f05fdae372d807719c1b841bfeccbf
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2019-07-17 11:23:59 +0530

    Add files via upload

---
+++ default parameter.py
@@ -0,0 +1,33 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> def getvalue(x,y):
+	return x+y
+
+>>> sum=getvalue(10,10)
+>>> sum
+20
+>>> 
+>>> sum*2
+40
+>>> sum+43
+63
+>>> 
+>>> 
+>>> 
+>>> 
+>>> def get_value()x,y=2:
+	
+SyntaxError: invalid syntax
+>>> 
+>>> def get_value(x,y=2):
+	print(x+y)
+
+	
+>>> get_value(10)
+12
+>>> 
+>>> get_value(10,12)
+22
+>>> 
+>>> 
