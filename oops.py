6a63d5e05af40dd63830b2ffea3486ea63a59687
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2019-07-29 11:01:36 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,18 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> class sample:
+	def getdata(self):
+		print("hi")
+
+		
+>>> s1=sample()
+>>> s1.getdata()
+hi
+>>> 
+>>> 
+>>> 
+>>> 
+>>> sample.getdata(s1)
+hi
+>>> 
+>>> 
