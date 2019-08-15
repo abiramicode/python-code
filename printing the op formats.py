a3cfb762c98723c15ddd971d6c3258af7d1bb6cd
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2019-08-15 12:07:10 +0530

    Add files via upload

---
+++ printing the op formats.py
@@ -0,0 +1,20 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> str="Abirami"
+>>> str
+'Abirami'
+>>> 
+>>> print("{}".format(str))
+Abirami
+>>> 
+>>> print("%s"%str)
+Abirami
+>>> 
+>>> print("",str)
+ Abirami
+>>> 
+>>> type(str)
+<class 'str'>
+>>> 
+
+>>> 
